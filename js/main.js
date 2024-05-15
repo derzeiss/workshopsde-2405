7f3eea152131267602d16ffd457bfcd565e54097
alex <alex.zeiss@gmx.net>
2024-05-15 14:00:41 +0200

    Add modules task

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import sayBye, { sayHello as sayHi } from './module.js';
+
+sayHi('John');
+sayBye('John');
+
+/*
+npm init -y
+npm i serve
+npm start
+*/
---
+++ js/module.js
@@ -0,0 +1,4 @@
+export const sayHello = (name) => console.log(`Hello ${name}`);
+
+const sayBye = (name) => console.log(`Bye ${name}`);
+export default sayBye;
